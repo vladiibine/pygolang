b7dd8003313ac2e1331b1ee55648ec3bb4e7a209
Vlad Ardelean <vlad.george.ardelean@gmail.com>
2020-10-16 15:44:34 +0300

    Slightly more thorough test for the if statement (tested negative evaluation)

--- tests/integration/test_conditional_stmt.py
+++ tests/integration/test_conditional_stmt.py
@@ -15,3 +15,21 @@ def test_simple_conditional():
 
     assert not io.stderr, io.format_stderr_for_debugging()
     assert io.stdout == ['2']
+
+
+def test_simple_if_conditional_twice():
+    io = FakeIO([
+        'x := 1',
+        'x',
+        'if true {x = 2}',
+        'x',
+        'if false {x = 3}',
+        'x',
+        'if true {x = 4}',
+        'x',
+    ])
+
+    main(io)
+
+    assert not io.stderr, io.format_stderr_for_debugging()
+    assert io.stdout == ['1', '2', '2', '4']
