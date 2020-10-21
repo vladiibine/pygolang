08505cdbf35245ab61357f9e3b7054b7519d68f1
Vlad Ardelean <vlad.george.ardelean@gmail.com>
2020-10-21 12:59:32 +0300

    Renaming refactor in stdlib.fmt

--- pygolang/stdlib/fmt.py
+++ pygolang/stdlib/fmt.py
@@ -1,4 +1,4 @@
-import io as python_io
+import io
 
 from pygolang import ast
 
@@ -18,7 +18,7 @@ class _PrintlnClass(ast.NativeFunction):
         :param pygolang.side_effects.SideEffects side_effects:
         :param dict[str,list[object,]] arguments:
         """
-        f = python_io.StringIO()
+        f = io.StringIO()
         print(arguments['a'][0].to_pygo_repr(), end='', file=f)
         side_effects.to_stdout(f.getvalue())
 
